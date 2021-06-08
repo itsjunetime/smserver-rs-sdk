74023c8a03ed657cdad5af364b2868a8319b78f3
iandwelker <iandwelker@gmail.com>
2021-06-08 10:22:25 -0600

    Removed unnecessary trait impls

--- src/models.rs
+++ src/models.rs
@@ -11,19 +11,6 @@ pub struct Conversation {
 	pub is_selected: bool
 }
 
-impl From<&serde_json::Map<String, serde_json::Value>> for Conversation {
-	fn from(val: &serde_json::Map<String, serde_json::Value>) -> Conversation {
-		Conversation {
-			display_name: val["display_name"].as_str().unwrap_or("Name").to_owned(),
-			chat_identifier: val["chat_identifier"].as_str().unwrap_or("").to_owned(),
-			latest_text: val["latest_text"].as_str().unwrap_or("").to_owned(),
-			has_unread: val["has_unread"].as_bool().unwrap_or(false),
-			addresses: val["addresses"].as_str().unwrap_or("").to_owned(),
-			is_selected: false,
-		}
-	}
-}
-
 #[derive(Debug, Deserialize)]
 pub struct Message {
 	pub guid: String,
@@ -43,37 +30,6 @@ pub struct Message {
 	pub chat_identifier: Option<String>,
 	#[serde(default)]
 	pub message_type: MessageType,
-}
-
-impl From<&serde_json::Map<String, serde_json::Value>> for Message {
-	fn from(val: &serde_json::Map<String, serde_json::Value>) -> Message {
-		Message {
-			guid: val["guid"].as_str().unwrap_or("").to_owned(),
-			date: val["date"].as_i64().unwrap_or(0),
-			balloon_bundle_id: val["balloon_bundle_id"].as_str().unwrap_or("").to_owned(),
-			cache_has_attachments: val["cache_has_attachments"].as_bool().unwrap_or(false),
-			imessage: val["service"].as_str().unwrap_or("") == "iMessage",
-			is_from_me: val["is_from_me"].as_bool().unwrap_or(false),
-			subject: val["subject"].as_str().unwrap_or("").to_owned(),
-			text: val["text"].as_str().unwrap_or("").to_owned(),
-			associated_message_guid: val["associated_message_guid"].as_str().unwrap_or("").to_owned(),
-			associated_message_type: val["associated_message_type"].as_i64().unwrap_or(0) as i16,
-			message_type: MessageType::Normal,
-			attachments: if val.contains_key("attachments") {
-				val["attachments"].as_array()
-					.unwrap()
-					.iter()
-					.map(|a| Attachment::from(a.as_object().unwrap()))
-					.collect()
-			} else {
-				Vec::new()
-			},
-			date_read: val.get("date_read").map(|d| d.as_i64().unwrap_or(0)),
-			sender: val.get("sender").map(|s| s.as_str().unwrap_or("").to_owned()),
-			chat_identifier:
-				val.get("chat_identifier").map(|c| c.as_str().unwrap_or("").to_owned())
-		}
-	}
 }
 
 impl Message {
@@ -136,13 +92,4 @@ pub struct Attachment {
 	pub mime_type: String,
 	#[serde(rename = "filename")]
 	pub path: String,
-}
-
-impl From<&serde_json::Map<String, serde_json::Value>> for Attachment {
-	fn from(val: &serde_json::Map<String, serde_json::Value>) -> Attachment {
-		Attachment {
-			mime_type: val["mime_type"].as_str().unwrap_or("image/jpeg").to_owned(),
-			path: val["filename"].as_str().unwrap_or("file.jpg").to_owned(),
-		}
-	}
 }
